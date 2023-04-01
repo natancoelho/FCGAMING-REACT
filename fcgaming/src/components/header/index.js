33d7f93c510433c47bd16754cf992ea1fc257448
Natan Coelho <natancoelho21@gmail.com>
2023-04-01 19:00:46 -0300

    v-3

--- fcgaming/src/components/header/index.js
+++ fcgaming/src/components/header/index.js
@@ -2,12 +2,28 @@ import { Link } from 'react-router-dom';
 import './style.css'
 
 
+
+
 function Header (){
     return(
         <div className='teste'>
         <header>
         
-            <h2> FC GAMING</h2>
+            <h2> 
+            <Link to="/">FC GAMING </Link>
+                
+
+            </h2>
+
+            <div className='icones'>
+
+            <Link to="https://www.instagram.com/niicolasmota/">
+                    <img className='discord' src='/imgs/discord.png'/>
+                    </Link>
+
+            </div>
+
+
             <div className='menu'>
                 <Link to="/">Home</Link>
                 <Link to="/sobre">Sobre</Link>
--- fcgaming/src/pages/Home/index.js
+++ fcgaming/src/pages/Home/index.js
@@ -7,8 +7,8 @@ function Home() {
 
     
     return (
-       
-        <div>
+       <body>
+    
             <div className='descricao'>
                 <div className='card1'>
                     <p className='desc1'>FC GAMING</p>
@@ -17,10 +17,15 @@ function Home() {
             </div>
 
             <div className='divisao2'>
+            
                 <div className='card2'>
+                <Link to="https://www.instagram.com/niicolasmota/">
+                    <img className='discord1' src='/imgs/discord.png'/>
+                    </Link>
                      <p className='desc3'>Venha fazer parte</p>
                      <p className='desc4'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in</p>
                  </div>
+                 
             </div>
 
             <div className='divisao3'>
@@ -85,8 +90,8 @@ function Home() {
                 Copyright 2023, FC GAMING, todos os direitos reservados
                 </p>
             </div>
-        </div>
-        
+      
+        </body>
     );
 }
  
