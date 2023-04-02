b382c0ee3dbf500b569ca693ad47a0f21fed100c
Natan Coelho <natancoelho21@gmail.com>
2023-04-02 00:34:46 -0300

    v-4

--- fcgaming/src/pages/Home/index.js
+++ fcgaming/src/pages/Home/index.js
@@ -17,19 +17,20 @@ function Home() {
             </div>
 
             <div className='divisao2'>
-            
                 <div className='card2'>
                 <Link to="https://www.instagram.com/niicolasmota/">
                     <img className='discord1' src='/imgs/discord.png'/>
                     </Link>
                      <p className='desc3'>Venha fazer parte</p>
                      <p className='desc4'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in</p>
+                        <button className='entrar'>
+                            Entrar no Discord
+                        <Link to="https://www.instagram.com/niicolasmota/"></Link>
+                        </button>
                  </div>
-                 
-            </div>
-
-            <div className='divisao3'>
+         
                 <div className='container'>
+                   
                     <div className='item1'>
                         <div className='foto1'>
                             
